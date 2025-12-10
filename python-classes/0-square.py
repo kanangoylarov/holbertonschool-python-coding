fb113c4f3a8bb7f4f68c4a288601ce396849f06f
kanangoylarov <goylerov03@gmail.com>
2025-12-10 17:27:09 +0000

    blank line error

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """square class
 """
+
+
 class Square:
-
-
     """square size class
     """
     __size = 0
 
     def __init__(self, size):
-
-
         """class rewiews
         """
         self.__size = size
