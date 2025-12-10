fa6d46fef31f3ef819ba24c8d27311198d6d5272
kanangoylarov <goylerov03@gmail.com>
2025-12-10 17:25:33 +0000

    just solve please

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,11 +2,15 @@
 """square class
 """
 class Square:
+
+
     """square size class
     """
     __size = 0
 
     def __init__(self, size):
+
+
         """class rewiews
         """
         self.__size = size
