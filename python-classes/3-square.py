673973183b0d949cf7af7248b580abeb1ec2c6f0
kanangoylarov <goylerov03@gmail.com>
2025-12-13 09:19:52 +0000

    corrected inentation error

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -22,17 +22,17 @@ class Square:
 
         return self.__size * self.__size
 
-        @property
-        def size(self):
-            """getter of size"""
+    @property
+    def size(self):
+        """getter of size"""
 
-            return self.__size
-        @size.setter
-        def size(self,value):
-            """setter of value"""
+        return self.__size
+    @size.setter
+    def size(self,value):
+        """setter of value"""
 
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
