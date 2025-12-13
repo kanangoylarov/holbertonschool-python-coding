58e690759c9ce17c020f2e0dcc5996d1ee671791
kanangoylarov <goylerov03@gmail.com>
2025-12-13 08:52:41 +0000

    pep validarion added

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -8,11 +8,11 @@ class Square:
     """
     __size = 0
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """class rewiews
         """
         self.__size = size
-        if type(self.__size) != int:
+        if type(self.__size) is not int:
             raise TypeError("size must be an integer")
         if self.__size < 0:
             raise ValueError("size must be >= 0")
