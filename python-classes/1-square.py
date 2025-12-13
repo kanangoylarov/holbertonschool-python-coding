6d43352a5bb613ab720488b51e999a51da320f22
kanangoylarov <goylerov03@gmail.com>
2025-12-13 08:40:10 +0000

    size validation

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""square class
+"""
+
+
+class Square(Exception):
+    """square size class error
+    """
+    __size = 0
+
+    def __init__(self, size):
+        """class rewiews
+        """
+        self.__size = size
+        if type(self.size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
